417a41c6cab9b5949a02dcee19ef1d03cd6ad6b6
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-09-02 15:14:51 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Flint and Flame
+========
+
+A recreation of an Ember tutorial taught on [Code School](http://ember.codeschool.com).
---
+++ js/app.js
@@ -0,0 +1,13 @@
+var App = Ember.Application.create({
+	LOG_TRANSITIONS: true;
+});
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
