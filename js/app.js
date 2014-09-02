22007a72c72dc97cf6e0f62d7e910247342738e3
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-09-02 15:52:05 -0700

    learned routes and templates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 var App = Ember.Application.create({
-	LOG_TRANSITIONS: true;
+	LOG_TRANSITIONS: true
 });
 
 App.Router.map(function() {
-  // put your routes here
+  this.route('about', {path: '/aboutus'}); // {} not needed if path is same as route. Index behavior is loaded as default
+  this.route('buy', {path:'/random-name'})
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue'];
+//   }
+// });
