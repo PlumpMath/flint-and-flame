8e2d12a17ceb88b66217fa70564fd9d9844039d3
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-09-12 11:25:30 -0700

    added explantory notes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
+/*global Ember */
+'use strict';
+
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 App.Router.map(function() {
-  this.route('credits', { path: '/thanks' });
+  this.route('credits', {
+    path: '/thanks'
+  }); // the name (href and LOG_TRANSITIONS) is 'credits', the URL is 'thanks'
   this.route('about');
   this.resource('products');
 });
+// you must call .property() at the end of any method to call it in a controller. See productsCount().property() for proof
+// a method cannot access a variable that is declared after the method: ex. productsCount() could not return App.PRODUCTS.length
 App.IndexController = Ember.Controller.extend({
-  productsCount: 6,
+  productsCount: function() {
+    return App.PRODUCTS.length;
+  }.property(),
   logo: 'images/logo-small.png',
   time: function() {
     return (new Date()).toDateString();
@@ -16,29 +25,38 @@ App.IndexController = Ember.Controller.extend({
 App.AboutController = Ember.Controller.extend({
   contactName: 'Anostagia',
   avatar: 'images/avatar.png',
-  open: function() {
-    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
-  }.property()
+  day: function() {
+    var today = (new Date()).getDay();
+    return this.days[today];
+  }.property(),
+  days: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
 });
 
+
+// This resourse route returns a resource, in this case App.products which is intuitively tied to the model method. Can call {{#each}} on the model
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
     return App.PRODUCTS;
   }
 });
-App.PRODUCTS = [
-  {
-    title: 'Flint',
-    price: 99,
-    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
-    isOnSale: true,
-    image: 'images/products/flint.png'
-  },
-  {
-    title: 'Kindling',
-    price: 249,
-    description: 'Easily combustible small sticks or twigs used for starting a fire.',
-    isOnSale: false,
-    image: 'images/products/kindling.png'
-  }
-];
+App.PRODUCTS = [{
+  title: 'Flint',
+  price: 99,
+  description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+  isOnSale: true,
+  image: 'images/products/flint.png'
+}, {
+  title: 'Kindling',
+  price: 249,
+  description: 'Easily combustible small sticks or twigs used for starting a fire.',
+  isOnSale: false,
+  image: 'images/products/kindling.png'
+}];
